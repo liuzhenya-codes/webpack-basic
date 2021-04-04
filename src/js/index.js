6002c4e5208318f45ae756c2f8343519d366d9c4
liuzhenya <1965350918@qq.com>
2021-04-04 17:43:31 +0800

    '比记更新'

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
 import { func1 } from './tree-shaking'
 
 func1(123)
+
+function trim (string) {
+  // 先将字符串转为数组
+  const stringToArray = string.split('')
+  // 清除数组左边的空白项
+  const leftNulIndex = stringToArray.findIndex(char => char !== ' ')
+  stringToArray.splice(0, leftNulIndex)
+  // 清除数组右边的空白项
+  stringToArray.reverse()
+  const rightNulIndex = stringToArray.findIndex(char => char !== ' ')
+  stringToArray.splice(0, rightNulIndex)
+  // 将数组顺序调整回来
+  stringToArray.reverse()
+  // 将调整完毕的数组重新转为字符串，即为最终结果
+  const result = stringToArray.join('')
+  return result
+}
